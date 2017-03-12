6d0b1ca969e4fa1999ef061685b31b26e0035a68
Sallmir <asp.ravil@gmail.com>
2017-03-12 15:05:03 +0200

    Update FindDistance.java

--- EasyStartInItHomeWorks/src/HomeWork1/FindDistance.java
+++ EasyStartInItHomeWorks/src/HomeWork1/FindDistance.java
@@ -5,12 +5,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class FindDistance {
     public static void main(String[] args) throws IOException {
 
-        ArrayList<Integer> numbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<Integer>();
 
         inputNumbers(numbers);
 
@@ -20,7 +21,7 @@ public class FindDistance {
 
     }
 
-    public static void inputNumbers(ArrayList<Integer> numbers) throws IOException {
+    public static void inputNumbers(List<Integer> numbers) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String number = "0";
 
@@ -35,21 +36,21 @@ public class FindDistance {
         }
     }
 
-    public static boolean checkString(String string) {
+    public static boolean checkString(String chekingInput) {
 
-        if (string == null || string.length() == 0) return false;
+        if (chekingInput == null || chekingInput.length() == 0) return false;
 
         int i = 0;
-        if (string.charAt(0) == '-') {
-            if (string.length() == 1) {
+        if (chekingInput.charAt(0) == '-') {
+            if (chekingInput.length() == 1) {
                 return false;
             }
             i = 1;
         }
 
         char c;
-        for (; i < string.length(); i++) {
-            c = string.charAt(i);
+        for (; i < chekingInput.length(); i++) {
+            c = chekingInput.charAt(i);
             if (!(c >= '0' && c <= '9')) {
                 return false;
             }
@@ -57,32 +58,29 @@ public class FindDistance {
         return true;
     }
 
-    public static void minNumbers(ArrayList<Integer> numbers) {
+    public static void minNumbers(List<Integer> numbers) {
         int min = numbers.get(0);
-        int min2 = numbers.get(0);
-        int indEl = 0;
-        int indEl2 = 0;
+        int min2 = numbers.get(numbers.size()-1);
+        int indEl = numbers.indexOf(min);
+        int indEl2 = numbers.indexOf(min2);
 
         for (int i = 0; i < numbers.size(); i++) {
-
-            if (min > numbers.get(i)) {
-                min = numbers.get(i);
-                indEl = i;
-
-            }
+            min = (min > numbers.get(i)? numbers.get(i): min);
+            indEl = numbers.indexOf(min);
+//            min2 = (min2 > numbers.get(i) && indEl != i? numbers.get(i): min2);
+//            indEl2 = numbers.indexOf(min2);
         }
         for (int i = 0; i < numbers.size(); i++) {
 
             if ((min2 > numbers.get(i)) && (indEl != i)) {
-                min2 = numbers.get(i);
-                indEl2 = i;
+               min2 = numbers.get(i);
+               indEl2 = i;
             }
-        }
-
+      }
+        
             System.out.println("Min = "+min + " - index - " + indEl);
             System.out.println("Min2 = "+min2 + " - index2 - " + indEl2);
             System.out.println("Разница -  "+Math.abs(indEl-indEl2));
-
     }
 }
 
